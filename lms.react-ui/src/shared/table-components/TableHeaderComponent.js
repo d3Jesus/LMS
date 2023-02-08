ae547ce20d8eddc04103a4569333665c193fbf72
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-08 07:29:49 +0200

    Created table header component

---
+++ lms.react-ui/src/shared/table-components/TableHeaderComponent.js
@@ -0,0 +1,14 @@
+const TableHeader = ({ headers }) => {
+    return (
+        <thead>
+            <tr>
+                {
+                    headers.map(header =>
+                        <th key={header}>{header}</th>)
+                }
+            </tr>
+        </thead>
+    );
+}
+
+export default TableHeader;
