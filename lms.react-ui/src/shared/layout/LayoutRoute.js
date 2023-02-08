10a1a5aa64cda75ff7196085d159ebea3f4130af
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-08 08:06:57 +0200

    Deleted test components

--- lms.react-ui/src/shared/layout/LayoutRoute.js
+++ lms.react-ui/src/shared/layout/LayoutRoute.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "../Home";
+import Home from "./Home";
 import Authors from "../../pages/author/Authors";
 
 const Routing = () => {
