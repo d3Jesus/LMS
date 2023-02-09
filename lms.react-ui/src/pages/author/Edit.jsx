7a56d114a65c0f2184e2234a0d3f10c21eb3ca4c
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-09 11:31:38 +0200

    Handle author edit

--- lms.react-ui/src/pages/author/Edit.jsx
+++ lms.react-ui/src/pages/author/Edit.jsx
@@ -14,7 +14,7 @@ export default function EditAuthor(props) {
 
     useEffect(() => {
         const getAuthor = async () => {
-            const url = `https://localhost:7078/api/authors/4`;
+            const url = `https://localhost:7078/api/authors/${props.id}`;
             await fetch(url)
                 .then((response) => {
                     if (response.ok) {
@@ -38,53 +38,60 @@ export default function EditAuthor(props) {
         getAuthor()
     }, [props.id]);
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-    // const handleSubmit = async (event) => {
-    //   event.preventDefault();
+        try {
+            let res = await fetch("https://localhost:7078/api/authors", {
+                method: "PUT",
+                body: JSON.stringify({
+                    id: props.id,
+                    firstName: firstName,
+                    lastName: lastName,
+                    nationality: nationality
+                }),
+                mode: 'cors', cache: 'no-cache',
+                headers: {
+                    'Content-Type': "application/json"
+                }
+            });
+            let resJson = await res.json();
 
-    //   try {
-    //     let res = await fetch("https://localhost:7078/api/authors", {
-    //       method: "GET",
-    //       body: JSON.stringify({
-    //         id: props.id
-    //       }),
-    //       mode: 'cors', cache: 'no-cache',
-    //       headers: {
-    //         'Content-Type': "application/json"
-    //       }
-    //     });
-    //     let resJson = await res.json();
-    //     console.log(resJson)
-    //     if (resJson.succeeded) {
-    //       alert("Success");
-    //     } else {
-    //       console.log(resJson.message);
-    //       alert(resJson.message);
-    //     }
-    //   } catch (err) {
-    //     console.log(err);
-    //   }
-    // }
+            if (resJson.succeeded) {
+                alert(resJson.message);
+            } else {
+                alert(resJson.message);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
     return (
         <Modal>
             <ModalHeader>
                 <h4 className="modal-title">Edit Author</h4>
-                <button type="button" onClick={props.close} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" onClick={props.close}
+                    className="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close">
+                </button>
             </ModalHeader>
             <ModalBody>
-                <Form /*onSubmit={handleSubmit}*/>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3">
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder="John" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                        <Form.Control type="text" placeholder="John"
+                            value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Doe" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                        <Form.Control type="text" placeholder="Doe"
+                            value={lastName} onChange={(e) => setLastName(e.target.value)} />
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Nationality</Form.Label>
-                        <Form.Control type="text" placeholder="Mozambican" value={nationality} onChange={(e) => setNationality(e.target.value)} />
+                        <Form.Control type="text" placeholder="Mozambican"
+                            value={nationality} onChange={(e) => setNationality(e.target.value)} />
                     </Form.Group>
                     <Button variant="outline-success" type="submit">
                         Submit
