6446edfd3757b253185ea1ce3fa963f4093852bf
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-13 10:05:56 +0200

    Render datatable

--- lms.react-ui/src/shared/layout/SpinnerComponent.jsx
+++ lms.react-ui/src/shared/layout/SpinnerComponent.jsx
@@ -1,7 +1,11 @@
 import Spinner from 'react-bootstrap/Spinner';
 
 const SpinnerComponent = () => {
-  return <Spinner animation="border" />;
+  return (
+    <div className='text-center'>
+      <Spinner animation="border" />
+    </div>
+  );
 }
 
 export default SpinnerComponent;
