40688678e80c3c3efb3e25ab6f48f24e59b25779
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-15 22:36:51 +0200

    Added reservation endpoints

---
+++ LMS/Controllers/ReservationController.cs
@@ -0,0 +1,62 @@
+﻿using LMS.Application.Interfaces;
+using LMS.Application.ViewModels.Reservation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/reservations")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationService _service;
+        public ReservationController(IReservationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddReservationViewModel model)
+        {
+            var response = await _service.AddAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(GetReservationViewModel model)
+        {
+            var response = await _service.UpdateAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == 0)
+                return BadRequest(id);
+
+            var reservation = await _service.GetByIdAsync(id);
+
+            if (reservation.ResponseData is null)
+                return NotFound();
+
+            var response = await _service.DeleteAsync(reservation.ResponseData);
+
+            return Ok(response);
+        }
+    }
+}
