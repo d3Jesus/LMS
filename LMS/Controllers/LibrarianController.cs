64fb0587e0caed917274728f13be6378e9127e52
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-15 22:35:22 +0200

    Added Librarian endpoints

---
+++ LMS/Controllers/LibrarianController.cs
@@ -0,0 +1,61 @@
+﻿using LMS.Application.Interfaces;
+using LMS.Application.ViewModels.Librarian;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/librarians")]
+    [ApiController]
+    public class LibrarianController : ControllerBase
+    {
+        private readonly ILibrarianService _service;
+        public LibrarianController(ILibrarianService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddLibrarianViewModel model)
+        {
+            var response = await _service.AddAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(GetLibrarianViewModel model)
+        {
+            var response = await _service.UpdateAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == 0)
+                return BadRequest(id);
+
+            var librarian = await _service.GetByIdAsync(id);
+
+            if (librarian.ResponseData is null)
+                return NotFound();
+
+            var response = await _service.DeleteAsync(librarian.ResponseData);
+
+            return Ok(response);
+        }
+    }
+}
