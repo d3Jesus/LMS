77a42af003fa71a00a82c64f8b8e5b750aeff793
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-15 22:34:39 +0200

    Added Bookcase endpoints

---
+++ LMS/Controllers/BookcaseController.cs
@@ -0,0 +1,62 @@
+﻿using LMS.Application.Interfaces;
+using LMS.Application.ViewModels.Bookcase;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/bookcases")]
+    [ApiController]
+    public class BookcaseController : ControllerBase
+    {
+        private readonly IBookcaseService _service;
+        public BookcaseController(IBookcaseService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBookcaseViewModel model)
+        {
+            var response = await _service.AddAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(GetBookcaseViewModel model)
+        {
+            var response = await _service.UpdateAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == 0)
+                return BadRequest(id);
+
+            var author = await _service.GetByIdAsync(id);
+
+            if (author.ResponseData is null)
+                return NotFound();
+
+            var response = await _service.DeleteAsync(author.ResponseData);
+
+            return Ok(response);
+        }
+    }
+}
