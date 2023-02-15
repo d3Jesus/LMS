de508147732f34ec0e08cefc3a1a54e31ef1f363
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-15 22:37:09 +0200

    Added stock endpoints

---
+++ LMS/Controllers/StockController.cs
@@ -0,0 +1,55 @@
+﻿using LMS.Application.Interfaces;
+using LMS.Application.ViewModels.Stock;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/stocks")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _service;
+        public StockController(IStockService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStockViewModel model)
+        {
+            var response = await _service.AddAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(GetStockViewModel model)
+        {
+            var response = await _service.UpdateAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == 0)
+                return BadRequest(id);
+
+            var stock = await _service.GetByIdAsync(id);
+
+            if (stock.ResponseData is null)
+                return NotFound();
+
+            var response = await _service.DeleteAsync(stock.ResponseData);
+
+            return Ok(response);
+        }
+    }
+}
