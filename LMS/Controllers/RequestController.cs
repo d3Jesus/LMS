c0847017ec86284ebe8050889ddee497e68b242c
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-15 22:36:29 +0200

    Added request endpoints

---
+++ LMS/Controllers/RequestController.cs
@@ -0,0 +1,61 @@
+﻿using LMS.Application.Interfaces;
+using LMS.Application.ViewModels.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/requests")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        private readonly IRequestService _service;
+        public RequestController(IRequestService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRequestViewModel model)
+        {
+            var response = await _service.AddAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(GetRequestViewModel model)
+        {
+            var response = await _service.UpdateAsync(model);
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == 0)
+                return BadRequest(id);
+
+            var request = await _service.GetByIdAsync(id);
+
+            if (request.ResponseData is null)
+                return NotFound();
+
+            var response = await _service.DeleteAsync(request.ResponseData);
+
+            return Ok(response);
+        }
+    }
+}
