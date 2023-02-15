df684eb5abf8464bc24de1c667faf7bd72deee4f
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-02-15 22:15:46 +0200

    Book endpoints created

--- LMS/Controllers/BookController.cs
+++ LMS/Controllers/BookController.cs
@@ -1,15 +1,16 @@
 ﻿using LMS.Application.Interfaces;
-using LMS.Application.ViewModels.Author;
+using LMS.Application.ViewModels.Book;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LMS.API.Controllers
 {
-    [Route("api/authors")]
+    [Route("api/books")]
     [ApiController]
-    public class AuthorController : ControllerBase
+    public class BookController : ControllerBase
     {
-        private readonly IAuthorService _service;
-        public AuthorController(IAuthorService service)
+        private readonly IBookService _service;
+        public BookController(IBookService service)
         {
             _service = service;
         }
@@ -26,29 +27,17 @@ namespace LMS.API.Controllers
             return Ok(await _service.GetByIdAsync(id));
         }
 
-        [HttpGet("{name:alpha}")]
-        public async Task<IActionResult> GetByName(string name)
-        {
-            return Ok(await _service.GetByNameAsync(name));
-        }
-
-        [HttpGet("{nationality}")]
-        public async Task<IActionResult> GetByNationality(string nationality)
-        {
-            return Ok(await _service.GetByNationalityAsync(nationality));
-        }
-
         [HttpPost]
-        public async Task<IActionResult> Add(AddAuthorViewModel model)
-        {   
+        public async Task<IActionResult> Add(AddBookViewModel model)
+        {
             var response = await _service.AddAsync(model);
 
             return Ok(response);
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(GetAuthorViewModel model)
-        {   
+        public async Task<IActionResult> Update(GetBookViewModel model)
+        {
             var response = await _service.UpdateAsync(model);
 
             return Ok(response);
