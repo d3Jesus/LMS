0471b3010bc78aad8afb261c83778efefb6246b9
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-05-13 17:33:22 +0200

    Implemente GetAll category endpoint

--- LMS.Application/Interfaces/ICategoryService.cs
+++ LMS.Application/Interfaces/ICategoryService.cs
@@ -8,7 +8,8 @@ namespace LMS.Application.Interfaces
         Task<ServiceResponse<GetCategoryDto>> CreateAsync(AddCategoryDto category);
         Task<ServiceResponse<GetCategoryDto>> UpdateAsync(GetCategoryDto category);
         Task<ServiceResponse<bool>> DeleteAsync(int id);
-        Task<ServiceResponse<GetCategoryDto>> GetAsync(int id);
+        Task<ServiceResponse<IEnumerable<GetCategoryDto>>> GetAsync();
+        Task<ServiceResponse<GetCategoryDto>> GetByAsync(int id); 
         Task<ServiceResponse<IEnumerable<GetCategoryDto>>> GetAsync(string categoryName);
     }
 }
---
+++ LMS/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using LMS.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _categoryService.GetAsync());
+        }
+    }
+}
