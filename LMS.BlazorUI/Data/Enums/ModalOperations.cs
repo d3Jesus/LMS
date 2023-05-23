b1796c2980e19177f1adbe5d2d079a52281d3896
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-05-23 17:28:02 +0200

    Make display modal funtion "generic"

---
+++ LMS.BlazorUI/Data/Enums/ModalOperations.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS.BlazorUI.Data.Enums
+{
+    public static class ModalOperations
+    {
+        public const string ADD = "Add";
+        public const string UPDATE = "Update";
+        public const string DELETE = "Delete";
+    }
+}
