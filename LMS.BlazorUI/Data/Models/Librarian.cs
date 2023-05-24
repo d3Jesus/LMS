9619fda64c1186d5fe4aa045932489ca67950be6
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-05-24 16:30:08 +0200

    Added delete, update and add pages for librarian

---
+++ LMS.BlazorUI/Data/Models/Librarian.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.BlazorUI.Data.Models
+{
+    public class Librarian : Person
+    {
+        private string _address;
+        private string _email;
+        private string _phoneNumber;
+
+        [Required]
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+
+        [Required]
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        [Required]
+        [EmailAddress]
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+    }
+}
