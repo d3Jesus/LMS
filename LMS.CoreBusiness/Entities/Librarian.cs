6f366e6058964dadca1a7df5bdb92835ad04eb4a
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-09-09 23:08:12 +0200

    Use auto peroperty in librarian model.

--- LMS.CoreBusiness/Entities/Librarian.cs
+++ LMS.CoreBusiness/Entities/Librarian.cs
@@ -1,29 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace LMS.CoreBusiness.Entities
+﻿namespace LMS.CoreBusiness.Entities
 {
     public class Librarian : Person
     {
-        private string _address;
-        private string _email;
-        private string _phoneNumber;
+        public string PhoneNumber { get; set; }
 
-        public string PhoneNumber
-        {
-            get { return _phoneNumber; }
-            set { _phoneNumber = value; }
-        }
+        public string Address { get; set; }
 
-        public string Address 
-        {
-            get { return _address;  }
-            set { _address = value; } 
-        }
-        
-        public string Email 
-        {
-            get { return _email; }
-            set { _email = value; }
-        }
+        public string Email { get; set; }
     }
 }
--- LMS.CoreBusiness/Entities/Person.cs
+++ LMS.CoreBusiness/Entities/Person.cs
@@ -4,34 +4,12 @@ namespace LMS.CoreBusiness.Entities;
 
 public abstract class Person
 {
-    private int _id;
-    private string _firstName;
-    private string _lastName;
-    private bool _wasDeleted;
-    
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id
-    {
-        get { return _id; }
-        set { _id = value; }
-    }
+    public int Id { get; set; }
 
-    public string FirstName
-    {
-        get { return _firstName; }
-        set { _firstName = value; }
-    }
+    public string FirstName { get; set; }
 
-    public string LastName
-    {
-        get { return _lastName; }
-        set { _lastName = value; }
-    }
+    public string LastName { get; set; }
 
-    public bool WasDeleted
-    {
-        get { return _wasDeleted; }
-        set { _wasDeleted = value; }
-    }
+    public bool WasDeleted { get; set; }
 
 }
