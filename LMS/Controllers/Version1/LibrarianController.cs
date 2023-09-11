11165f352b8f1396c28f2e72b382912b7a20ce28
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-09-11 22:01:58 +0200

    Refactor: add versioning

---
+++ LMS/Controllers/Version1/LibrarianController.cs
@@ -0,0 +1,71 @@
+﻿using LMS.Application.Interfaces;
+using LMS.Application.ViewModels.Librarian;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers.Version1
+{
+    [Route("api/v1/librarians")]
+    [ApiController]
+    public class LibrarianController : ControllerBase
+    {
+        private readonly ILibrarianService _service;
+        public LibrarianController(ILibrarianService service) => _service = service;
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<GetLibrarianDto>))]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAsync(false));
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetLibrarianDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id <= 0) return BadRequest("Invalid librarian identifier.");
+
+            var response = await _service.GetByAsync(id);
+
+            return response.ResponseData is null ? NotFound() : Ok(response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(GetLibrarianDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Add(AddLibrarianDto librarianDto)
+        {
+            if (librarianDto == null) return BadRequest("Provide the required librarian information.");
+
+            var response = await _service.CreateAsync(librarianDto);
+
+            return response.ResponseData is not null ? CreatedAtAction(nameof(Get), new { response.ResponseData.Id }) : BadRequest(response);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetLibrarianDto))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(UpdateLibrarianDto librarianDto)
+        {
+            if (librarianDto == null) return BadRequest("Provide the required librarian information.");
+
+            var response = await _service.UpdateAsync(librarianDto);
+
+            return response.ResponseData is not null ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == 0) return BadRequest(id);
+
+            var response = await _service.DeleteAsync(id);
+
+            return response.ResponseData ? NoContent() : BadRequest(response);
+        }
+    }
+}
