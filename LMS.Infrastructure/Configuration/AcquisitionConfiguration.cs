222e967f984c1ddde91da8f694dfffb7a82ab302
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-09-12 21:03:05 +0200

    Create acquisitions model configurations

---
+++ LMS.Infrastructure/Configuration/AcquisitionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using LMS.CoreBusiness.Entities;
+
+namespace LMS.Infrastructure.Configurations;
+
+public class AcquisitionConfiguration : IEntityTypeConfiguration<Acquisition>
+{
+    public void Configure(EntityTypeBuilder<Acquisition> builder)
+    {
+        builder.ToTable("Acquisitions");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.LibrarianId).IsRequired();
+        builder.Property(x => x.DateRegistered).IsRequired();
+        builder.Property(x => x.Status).HasMaxLength(10).IsRequired();
+    }
+}
---
+++ LMS.Infrastructure/Configuration/AcquisitionItemsConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using LMS.CoreBusiness.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LMS.Infrastructure.Configurations;
+
+public class AcquisitionItemsConfiguration : IEntityTypeConfiguration<AcquisitionItems>
+{
+    public void Configure(EntityTypeBuilder<AcquisitionItems> builder)
+    {
+        builder.ToTable("AcquisitionItems");
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.AcquisitionId).IsRequired();
+        builder.Property(b => b.BookId).IsRequired();
+        builder.Property(b => b.Quantity).IsRequired();
+        builder.Property(b => b.PurchasePrice).IsRequired().HasColumnType("numeric").HasPrecision(18, 2);
+        builder.Property(b => b.SalePrice).IsRequired().HasColumnType("numeric").HasPrecision(18, 2);
+    }
+}
