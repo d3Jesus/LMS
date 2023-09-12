ad1b8cbab41b1e1db33b3cfb7dc72aafed849dc5
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-09-12 21:04:32 +0200

    Created acquisition service

---
+++ LMS.Application/Interfaces/IAcquisitionService.cs
@@ -0,0 +1,30 @@
+﻿
+using LMS.Application.ViewModels;
+using LMS.Application.ViewModels.Acquisition;
+
+namespace LMS.Application.Interfaces;
+
+public interface IAcquisitionService
+{
+    /// <summary>
+    /// Register a new acquisition
+    /// </summary>
+    /// <param name="acquisitionDto">Acquisition information.</param>
+    /// <returns></returns>
+    Task<ServiceResponse<GetAcquisitionDto>> CreateAsync(AddAcquisitionDto acquisitionDto);
+
+    /// <summary>
+    /// Retrieves all acquisitions in a range of date.
+    /// </summary>
+    /// <param name="fromDate">From date</param>
+    /// <param name="toDate">To date</param>
+    /// <returns>List of acquisitions in a given range of date.</returns>
+    Task<ServiceResponse<List<GetAcquisitionDto>>> GetAsync(DateTime fromDate, DateTime toDate);
+
+    /// <summary>
+    /// Retrieves acquisition with the given ID.
+    /// </summary>
+    /// <param name="acquisitionId">Acquisition ID.</param>
+    /// <returns></returns>
+    Task<ServiceResponse<GetAcquisitionDto>> GetAsync(string acquisitionId);
+}
---
+++ LMS.Application/Services/AcquisitionService.cs
@@ -0,0 +1,72 @@
+﻿using Mapster;
+using LMS.Application.Interfaces;
+using LMS.Application.ViewModels.Acquisition;
+using LMS.CoreBusiness.Entities;
+using LMS.CoreBusiness.Interfaces;
+using LMS.Application.ViewModels;
+
+namespace LMS.Application.Services;
+
+public class AcquisitionService : IAcquisitionService
+{
+    private readonly IAcquisition _repository;
+
+    public AcquisitionService(IAcquisition repository) => _repository = repository;
+
+    public async Task<ServiceResponse<GetAcquisitionDto>> CreateAsync(AddAcquisitionDto acquisitionDto)
+    {
+        try
+        {
+            Acquisition acquisition = acquisitionDto.Adapt<Acquisition>();
+
+            acquisition.Items = acquisitionDto.Items.Adapt<List<AcquisitionItems>>();
+            var result = await _repository.CreateAsync(acquisition);
+
+            if (result is null)
+                return new ServiceResponse<GetAcquisitionDto>
+                {
+                    Succeeded = false,
+                    Message = "Failed to register acquisition."
+                };
+
+            return new ServiceResponse<GetAcquisitionDto>
+            {
+                ResponseData = result.Adapt<GetAcquisitionDto>(),
+                Succeeded = true,
+                Message = "Acquisition registered successfuly."
+            };
+        }
+        catch
+        {
+            return new ServiceResponse<GetAcquisitionDto>
+            {
+                Succeeded = false,
+                Message = "An error occored while registering acquisition."
+            };
+        }
+    }
+
+    public async Task<ServiceResponse<List<GetAcquisitionDto>>> GetAsync(DateTime fromDate, DateTime toDate)
+    {
+        var response = await _repository.GetAsync(fromDate, toDate);
+
+        return new ServiceResponse<List<GetAcquisitionDto>>
+        {
+            ResponseData = response.Adapt<List<GetAcquisitionDto>>(),
+            Succeeded = true,
+            Message = string.Empty
+        };
+    }
+
+    public async Task<ServiceResponse<GetAcquisitionDto>> GetAsync(string acquisitionId)
+    {
+        var result = await _repository.GetAsync(acquisitionId);
+
+        return new ServiceResponse<GetAcquisitionDto>
+        {
+            ResponseData = result.Adapt<GetAcquisitionDto>(),
+            Succeeded = true,
+            Message = string.Empty
+        };
+    }
+}
