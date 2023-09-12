8728acb5de5c68298d2311e10f13de91f226a80c
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-09-12 21:02:35 +0200

    Created acquisition models

---
+++ LMS.CoreBusiness/Entities/Acquisition.cs
@@ -0,0 +1,16 @@
+﻿
+namespace LMS.CoreBusiness.Entities;
+
+public class Acquisition
+{
+    public string Id { get; set; }
+
+    public int LibrarianId { get; set; }
+    public virtual Librarian Librarian { get; set; }
+
+    public DateTime DateRegistered { get; init; } = DateTime.Now;
+
+    public string Status { get; set; }
+
+    public virtual ICollection<AcquisitionItems> Items { get; set; }
+}
---
+++ LMS.CoreBusiness/Entities/AcquisitionItems.cs
@@ -0,0 +1,21 @@
+﻿
+namespace LMS.CoreBusiness.Entities;
+
+public class AcquisitionItems
+{
+    public int Id { get; set; }
+
+    public string AcquisitionId { get; set; }
+    public virtual Acquisition Acquisition { get; set; }
+
+    public int BookId { get; set; }
+    public virtual Book Book { get; set; }
+    
+    public int Quantity { get; set; }
+    
+    public decimal PurchasePrice { get; set; }
+    
+    public decimal SalePrice { get; set; }
+    
+    public decimal GrossPrice { get; set; }
+}
