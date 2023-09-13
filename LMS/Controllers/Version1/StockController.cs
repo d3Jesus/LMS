7b2337c5de4f324bd26334bf05f2cf93742f39df
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-09-13 12:15:31 +0200

    Fix stock repository: add versioning

---
+++ LMS/Controllers/Version1/StockController.cs
@@ -0,0 +1,32 @@
+﻿using LMS.Application.Interfaces;
+using LMS.Application.ViewModels.Stock;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS.API.Controllers
+{
+    [Route("api/v1/stocks")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _service;
+        public StockController(IStockService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _service.GetAsync());
+        }
+
+        [HttpGet("{bookId:int}")]
+        public async Task<IActionResult> Get(int bookId)
+        {
+            if (bookId <= 0) return BadRequest("Invalid book ID");
+
+            var response = await _service.GetAsync(bookId);
+            return response.ResponseData is null ? NotFound(response.Message) : Ok(response);
+        }
+    }
+}
