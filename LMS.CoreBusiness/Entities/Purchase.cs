db1fdbaf4ddc8358105c6f31b2f80e45d5fa4379
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-09-13 13:49:45 +0200

    Refactor Purchase: use auto property

--- LMS.CoreBusiness/Entities/Purchase.cs
+++ LMS.CoreBusiness/Entities/Purchase.cs
@@ -2,44 +2,18 @@
 
 public class Purchase
 {
-    private int _id;
-    private int _librarianId;
-    private string _customerName;
-    private DateTime _datePurchased;
-    private decimal _totalPayed;
-    
     // many-to-many relationship with Book
     public virtual IList<PurchaseItems> PurchaseItems { get; set; }
     public virtual List<Librarian> Librarians { get; set; }
 
-    public int Id
-    {
-        get { return _id; }
-        set { _id = value; }
-    }
+    public int Id { get; set; }
 
-    public int LibrarianId
-    {
-        get { return _librarianId; }
-        set { _librarianId = value; }
-    }
+    public int LibrarianId { get; set; }
 
-    public string CustomerName
-    {
-        get { return _customerName; }
-        set { _customerName = value; }
-    }
+    public string CustomerName { get; set; }
 
-    public DateTime DatePurchased
-    {
-        get { return _datePurchased; }
-        private init { _datePurchased = DateTime.Now; }
-    }
+    public DateTime DatePurchased { get; set; } =DateTime.Now;
 
-    public decimal TotalPayed
-    {
-        get { return _totalPayed; }
-        set { _totalPayed = value; }
-    }
+    public decimal TotalPayed { get; set; }
 
 }
--- LMS.CoreBusiness/Entities/PurchaseItems.cs
+++ LMS.CoreBusiness/Entities/PurchaseItems.cs
@@ -3,41 +3,15 @@ namespace LMS.CoreBusiness.Entities;
 
 public class PurchaseItems
 {
-    private int _purchaseId;
-    private int _bookId;
-    private int _numberOfCopies;
-    private decimal _unitPrice;
-    private decimal _grossPrice;
+    public int PurchaseId { get; set; }
+    public int BookId { get; set; }
 
-    public int PurchaseId
-    {
-        get { return _purchaseId; }
-        set { _purchaseId = value; }
-    }
-    public int BookId
-    {
-        get { return _bookId; }
-        set { _bookId = value; }
-    }
+    public int NumberOfCopies { get; set; }
 
-    public int NumberOfCopies
-    {
-        get { return _numberOfCopies; }
-        set { _numberOfCopies = value; }
-    }
+    public decimal UnitPrice { get; set; }
 
-    public decimal UnitPrice
-    {
-        get { return _unitPrice; }
-        set { _unitPrice = value; }
-    }
+    public decimal GrossPrice { get; set; }
 
-    public decimal GrossPrice
-    {
-        get { return _grossPrice; }
-        set { _grossPrice = value; }
-    }
-    
     public Book Book { get; set; }
     public Purchase Purchase { get; set; }
 }
