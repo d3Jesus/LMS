9d64e71c94e55f838f02fbc6e5871d1daf712cd8
Yuran Cumbane <yuran.dejesus@gmail.com>
2023-09-14 16:14:39 +0200

    Added librarian ID and date created to users table

---
+++ LMS.CoreBusiness/Entities/Accounts/AspNetUsers.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace LMS.CoreBusiness.Entities.Accounts;
+
+public class AspNetUsers : IdentityUser
+{
+    public int LibrarianId { get; set; }
+    public virtual Librarian Librarian { get; set; }
+    
+    public DateTime DateCreated { get; set; }
+}
